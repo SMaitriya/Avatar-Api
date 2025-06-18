17618b96ea4649320fa35fd55d49673a47071796
MAITRIYA SRAMANER <maitriya@outlook.fr>
2025-06-18 17:24:30 +0200

    sunctum and api inscription

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,3 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/bibliotheque', [BibliothequeController::class, 'index'])->name('bibliotheque');
-}); /// A modifier car auth n'existe plus depuis breeze
-
