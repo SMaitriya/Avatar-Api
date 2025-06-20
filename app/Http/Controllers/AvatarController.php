356275c347cdd75f8a53078c386c1637f82d8914
MAITRIYA SRAMANER <maitriya@outlook.fr>
2025-06-20 16:10:23 +0200

    API avatar

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -12,7 +12,7 @@ class AvatarController extends Controller
         $fields = $request->validate([
             'name' => 'required|string|max:50',
             'nose_size' => 'required|string|max:50',
-            'eye_type' => 'nullable|string|max:50',
+            'eye_type' => 'required|string|max:50',
             'eye_color' => 'required|string|max:50',
             'eye_size' => 'required|string|max:50',
             'eyebrow_type' => 'required|string|max:50',
@@ -44,27 +44,85 @@ class AvatarController extends Controller
         ]);
     }
 
-    protected function generateSvg(array $fields)
-    {
-        
-        $svg = new \DOMDocument();
-        $baseSvgPath = public_path('svg/templates/base.svg');
-        if (!file_exists($baseSvgPath)) {
-            throw new \Exception('Template SVG manquant');
-        }
-        $svg->load($baseSvgPath);
-
-    
-
-        $hair = $svg->getElementById('hair');
-        $hairPath = public_path("svg/templates/hairs/{$fields['hair_type']}.svg");
-        $hairUrl = asset("svg/templates/hairs/{$fields['hair_type']}.svg"); // URL relative
-        if ($hair && file_exists($hairPath)) {
-            $hair->setAttribute('xlink:href', $hairUrl);
-            $hair->setAttribute('fill', $fields['hair_color']);
-        }
-
-        return $svg->saveXML();
-    
+   protected function generateSvg(array $fields)
+{
+    $svg = new \DOMDocument();
+    $baseSvgPath = public_path('svg/templates/base.svg');
+    if (!file_exists($baseSvgPath)) {
+        throw new \Exception('Template SVG manquant');
     }
+    $svg->load($baseSvgPath);
+    $this->addEyes($svg, $fields);
+    $this->setSkinColor($svg, $fields['skin_color']);
+    $this->addMouth($svg, $fields);
+
+
+
+    return $svg->saveXML();
+}
+
+protected function getElementById(\DOMDocument $doc, string $id)
+{
+    $xpath = new \DOMXPath($doc);
+    return $xpath->query("//*[@id='$id']")->item(0);
+}
+
+
+ protected function setSkinColor(\DOMDocument $svg, string $color)
+{
+    $faceElement = $this->getElementById($svg, 'face');
+    if ($faceElement) {
+        $faceElement->setAttribute('fill', $color);
+    }
+}
+
+
+protected function addEyes(\DOMDocument $svg, array $fields)
+{
+
+    $eyesContainer = $this->getElementById($svg, 'eyes');
+    if (!$eyesContainer) return;
+
+    $eyesPath = public_path("svg/templates/eyes/{$fields['eye_type']}.svg"); // modifie le champ 'eye_type'
+    if (!file_exists($eyesPath)) return;
+
+    $eyesSvg = new \DOMDocument();
+    $eyesSvg->load($eyesPath);
+    $eyesSvg->preserveWhiteSpace = false;
+    $eyesSvg->formatOutput = true;
+
+    $eyeId = $fields['eye_type'] . '-eyes'; // ex : croix-eyes, sleepy-eyes, etc.
+    $eyesElements = $this->getElementById($eyesSvg, $eyeId);
+
+    if ($eyesElements) {
+        $importedEyes = $svg->importNode($eyesElements, true);
+        $eyesContainer->appendChild($importedEyes);
+    }
+}
+
+protected function addMouth(\DOMDocument $svg, array $fields)
+{
+    $mouthContainer = $this->getElementById($svg, 'mouth');
+    if (!$mouthContainer) return;
+
+    $mouthPath = public_path("svg/templates/mouth/{$fields['mouth_type']}.svg");
+    if (!file_exists($mouthPath)) return;
+
+    $mouthSvg = new \DOMDocument();
+    $mouthSvg->load($mouthPath);
+    $mouthSvg->preserveWhiteSpace = false;
+    $mouthSvg->formatOutput = true;
+
+    $mouthId = $fields['mouth_type'] . '-mouth';
+    $mouthElements = $this->getElementById($mouthSvg, $mouthId);
+
+    if ($mouthElements) {
+        $importedMouth = $svg->importNode($mouthElements, true);
+        $mouthContainer->appendChild($importedMouth);
+    }
+}
+
+
+
+
 }
